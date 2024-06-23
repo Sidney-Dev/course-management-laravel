8e92a28f8365f9f3fe596dbd14e014643eb99347
Elton Sidney Pedro de Sousa <sidneymelodia@Eltons-MBP.home>
2024-06-23 09:54:24 +0200

    added role capability and added to registration feature

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,8 +3,12 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Role;
+use App\Models\Course;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +17,25 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+        DB::table('role_user')->delete();
+        User::truncate();
+        Role::truncate();
+        Course::truncate();
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+
+        $createdUser = User::create([
+            'name' => "admin",
+            'email' => "admin@example.com",
+            'password' => Hash::make('password')
+        ]);
+
+        $adminRole = Role::create(['name' => 'admin']);
+        $createdUser->roles()->attach($adminRole);
+
+        $this->call([
+            CourseSeeder::class,
         ]);
     }
 }
